4f776b486f532eb2cd5e8e668bbd0399ab9cc482
Андрей <@DESKTOP-CPE46V2>
2024-11-05 09:42:09 +0300

    Добавьте файлы проекта.

---
+++ urok/urok.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+#include <windows.h>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <iomanip>
+#include "Header.h"
+using namespace std;
+
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(2151);
+    ofstream fout;
+    ifstream fin;
+    Student Ivan = { 1, "Рюрикович", "Иван", "Васильевич", 'м', 18, 1.99 };
+    Ivan.f = "Грозный";
+    cout_(Ivan);
+    fout_(Ivan);
+}
